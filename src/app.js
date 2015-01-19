37a61ed6e88f0a36e60e8321851cebca93f048c9
GuiPinto <me@guipinto.com>
2015-01-19 14:42:44 -0500

    More heroku fixes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,37 @@
-var WebSocketServer = require('ws').Server
-, http = require('http')
-, express = require('express')
-, exphbs  = require('express3-handlebars')
-, app = express()
-, routes = require('./routes/routes');;
+var http = require('http'),
+    express = require('express'),
+    exphbs  = require('express3-handlebars'),
+    websocket  = require('ws'),
+    routes = require('./routes/routes');
+
+var app = express();
+
+var WebSocketServer = websocket.Server;
 
 var server = http.createServer(app);
-
 server.listen(process.env.PORT || 3000);
 
 var wss = new WebSocketServer({server: server});
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  layoutsDir: __dirname + '/views/layouts/',
-  partialsDir: __dirname + '/views/partials/'
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
 }));
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'handlebars');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'handlebars');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 routes(app);
