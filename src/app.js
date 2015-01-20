25b11d4308956a5aa5756edbf236cafbd73d2cc5
GuiPinto <me@guipinto.com>
2015-01-19 23:47:30 -0500

    Added cors

--- src/app.js
+++ src/app.js
@@ -41,6 +41,13 @@ app.configure(function(){
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 });
 
 app.configure('development', function(){
